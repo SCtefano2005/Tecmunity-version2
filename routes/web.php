e877a7aca1ad5cf8d54753dfacc9eb683a041f95
Paulozzq <paulo.garfar@gmail.com>
2024-05-16 17:27:21 -0500

    hola

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
+});
 Route::get('/demo', function () {
-    return ('el que lee esto es cabro');
-
+    return 'el que lee esto es cabro';
+    
 //actual
 });
-});
+
