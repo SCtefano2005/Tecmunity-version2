b5d43c5d98c6ce600ee897c4147a1410a81e2f9e
Lione313 <dilan.gutierrez@tecsup.edu.pe>
2024-05-16 17:28:40 -0500

    Primer cabmio

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
 
 Route::get('/demo', function () {
-    return ('el que lee esto es cabro');
-
-//dadaadadadadad cabro el que lee esto 
+    return 'el que lee esto es cabro';
 });
+
+Route::get('/bas/demon', function () {
+    return 'el que lee esto es cabro';
 });
