00b63ddd93d2682cbc43fd494997e341bea9ea4b
Lione313 <dilan.gutierrez@tecsup.edu.pe>
2024-05-16 00:16:34 -0500

    Primer cambio

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::get('/demo', function () {
+    return ('el que lee esto es cabro');
+});
 });
