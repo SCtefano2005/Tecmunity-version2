045e613b960aef306d2bfb5df7d553583df7df39
SVillalva <mstefanovg@gmail.com>
2024-05-17 17:13:59 -0500

    Modelos y Migraciones(USUARIO, CARRERA)

---
+++ database/migrations/2024_05_17_220957_create_usuarios_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->nullable();
+            $table->string('apellido')->nullable();
+            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->date('fecha_nacimiento')->nullable();
+            $table->timestamp('fecha_registro')->nullable();
+            $table->string('sexo')->nullable();
+            $table->boolean('status');
+            $table->boolean('privado');
+            $table->boolean('admin');
+            $table->string('avatar')->nullable();
+            $table->unsignedBigInteger('carrera_id'); // Nombre de la columna de la clave foránea
+            $table->text('biografia')->nullable();
+            $table->timestamps();
+
+            // Definir la restricción de clave foránea
+            $table->foreign('carrera_id')->references('id')->on('carreras')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
